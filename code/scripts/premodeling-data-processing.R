93f110f520e2fe77d20a62eb442107dc5822598c
PhilHoon Oh <vlfgns5@gmail.com>
2016-10-27 16:13:37 -0700

    premodeling-data-processing.R: remove africanamerican column in new_credit and scaled_credit to void error

--- code/scripts/premodeling-data-processing.R
+++ code/scripts/premodeling-data-processing.R
@@ -13,25 +13,24 @@ ethnicity = credit$Ethnicity
 balance = credit$Balance
 
 
-#Dummy out categorical Variables one by one in order to apprioate name to colums
+#Dummy out categorical Variables one by one 
+#in order to name columns appropriately.
 temp_gender = model.matrix(balance~gender)
 temp_student = model.matrix(balance~student)
 temp_married = model.matrix(balance~marriage)
 temp_ethnicity = model.matrix(balance~ethnicity)
 
-#Removing column of ones, and appending Balance on categorical variables one by one for naming columns appropriately.
-#adding afriacnamerican column in new_ethnicity(instead of using 2 variables, using 3 variables for analysis)
+#Removing column of ones, and appending Balance on categorical variables one by one 
+#in order to name columns appropriately.
 new_gender = cbind(female = temp_gender[,-1], balance = balance)
 new_student = cbind(student = temp_gender[,-1], balance = balance)
 new_marriage = cbind(marriage = temp_gender[,-1], balance = balance)
-new_ethnicity = cbind(africanamerican = (temp_ethnicity[,2]+temp_ethnicity[,3]-1)*(-1)
-                      , asian = temp_ethnicity[,2], caucasian = temp_ethnicity[,3], balance = balance)
+new_ethnicity = cbind(asian = temp_ethnicity[,2], caucasian = temp_ethnicity[,3], balance = balance)
 
 #saving dummyout variables
 new_credit = data.frame(income, limit, rating, cards, age, education, gender = new_gender[,1], student = new_student[,1]
-                      , marriage=new_marriage[,1], africanamerican = new_ethnicity[,1]
-                      , asian = new_ethnicity[,2], caucasian = new_ethnicity[,3] 
-                      , balance = balance)
+                      , marriage=new_marriage[,1], asian = new_ethnicity[,2]
+                      , caucasian = new_ethnicity[,3], balance = balance)
 write.csv(new_credit, file ="../../data/new-credit.csv" )
 
 
