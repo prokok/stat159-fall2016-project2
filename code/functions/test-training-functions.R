54b604550766bc9571e8309ac4ac34c7a590fd4f
PhilHoon Oh <vlfgns5@gmail.com>
2016-10-29 17:16:54 -0700

    editing ridge-regression

--- code/functions/test-training-functions.R
+++ code/functions/test-training-functions.R
@@ -6,9 +6,7 @@ scaled_credit = read.csv("../../data/scaled-credit.csv", row.names=1)
 set.seed(0)
 sub = sample(1:400, 300, replace = FALSE)
 
-<<<<<<< HEAD
 save(test,train, file = "../../data/train-test.Rdata")
-=======
 new_credit_test = new_credit[sub,]
 new_credit_train = new_credit[-sub,]
 
@@ -17,4 +15,4 @@ scaled_credit_train = scaled_credit[-sub,]
 
 save(new_credit_test, new_credit_train, file = "../../data/new-train-test.Rdata")
 save(scaled_credit_test, scaled_credit_train, file = "../../data/scale-train-test.RData")
->>>>>>> philhoon
+
--- code/scripts/ridge-regression.R
+++ code/scripts/ridge-regression.R
@@ -1,21 +1,25 @@
-install.packages('glmnet')
 library(glmnet)
 load('data/new-train-test.RData')
 load('data/scale-train-test.RData')
-
-#x = matrix
-#y = vector
 
 
 grid <- 10^seq(10, -2, length = 100)
 x <- as.matrix(scaled_credit_train[,-length(scaled_credit_train)])
 y <- as.matrix(scaled_credit_train$balance)
 
+x_test <- as.matrix(scaled_credit_test[,-length(scaled_credit_train)])
+y_test <- as.matrix(scaled_credit_test$balance)
+
 cv.out <- cv.glmnet(x, y, alpha=0, lambda=grid, intercept=FALSE, standardize=FALSE)
 plot(cv.out)
 bestlambda <- cv.out$lambda.min
 
 
+ridge.pred = predict(cv.out, s=bestlambda, newx =x_test)
+mean(( ridge.pred -y_test)^2)
 
-predict (cv.out, type= "coefficients", s= bestlambda) [1:20,]
-  
+
+ridge.mod =glmnet (x[train ,],y[train],alpha =0, lambda =grid ,
+                   thresh =1e -12)
+ridge.pred=predict (ridge .mod ,s=4, newx=x_test)
+> mean(( ridge.pred -y.test)^2)
