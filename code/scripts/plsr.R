e306bbab56a0eb980b4a3463d456e73032837089
PhilHoon Oh <vlfgns5@gmail.com>
2016-10-31 11:13:45 -0700

    creating plsr.R

--- code/scripts/plsr.R
+++ code/scripts/plsr.R
@@ -1,33 +1,27 @@
-install.packages('pls')
 library(pls)
-set.seed(924)
-load('../../data/new-train-test.RData')
+load('../../data/scale-train-test.RData')
 load('../../data/scale-train-test.RData')
 scaled_credit = read.csv("../../data/scaled-credit.RData")
 
-#x[test,] = scaled_credit_test
-y.test <- scaled_credit_test$balance
-y.test_full <- scaled_credit$balance
+y.test = scaled_credit_test$balance
+y.test_full = scaled_credit$balance
 
-pcr.fit <- pcr(balance~., data = scaled_credit_train, validation='CV')
-validationplot(pcr.fit, val.type="MSEP")
+set.seed(0)
+pls.fit <- plsr(balance~., data = scaled_credit_train, validation='CV')
+summary(pls.fit)
+M <- which(pls.fit$validation$PRESS==min(pls.fit$validation$PRESS))
 
-#m = pcr.fit$validation$PRESS[which(min(pcr.fit$validation$PRESS))]
-#which(min(pcr.fit$validation$PRESS))
-
-#pcr.fit$validation$PRESS
-#min(pcr.fit$validation$PRESS)
-#str(pcr.fit$validation$PRESS)
+validationplot(pls.fit, val.type="MSEP")
 
 ##code to subset pcr.fit$validation$PRESS to the 10th parameter
-M <- which(pcr.fit$validation$PRESS==min(pcr.fit$validation$PRESS))
+m = as.numeric(which(pls.fit$validation$PRESS==min(pls.fit$validation$PRESS)))
 
 #ncomp = m  (in this case m=10)
-pcr.pred <- predict(pcr.fit, scaled_credit_test, ncomp=M)
+pls.pred <- predict(pls.fit, scaled_credit_test, ncomp=m)
 #find the MSE
 mean((pcr.pred - y.test)^2)
 
-
 #full prediction
-pcr.pred_full <- predict(pcr.fit, scaled_credit, ncomp =M)
-mean((pcr.pred_full - y.test_full)^2)
+pls.pred_full <- predict(pls.fit, scaled_credit, ncomp =m)
+mean((pls.pred_full - y.test_full)^2)
+
