42d8b7464deb07241a2c04f0bf189cbeb694e69f
PhilHoon Oh <vlfgns5@gmail.com>
2016-10-31 13:58:26 -0700

    creating ols.R

--- code/scripts/ols.R
+++ code/scripts/ols.R
@@ -1,13 +1,22 @@
 load('../../data/scale-train-test.RData')
 scaled_credit = read.csv("../../data/scaled-credit.RData")
 
-reg = lm(balance ~ income+limit+rating+cards+age+education+student
+income = credit$Income
+limit = credit$Limit
+rating = credit$Rating
+cards = credit$Cards
+age = credit$Age
+education = credit$Education
+gender = credit$Gender
+student = credit$Student
+marriage = credit$Married
+ethnicity = credit$Ethnicity
+balance = credit$Balance
+
+reg = lm(balance ~ income+limit+rating+cards+age+education
          , data = scaled_credit_train)
-cor(scaled_credit_train)
 sum_reg = summary(reg)
 
-test=cbind(scaled_credit_train$gender,scaled_credit_train$student,scaled_credit_train$marraige)
-cor(test)
 
 
 save(reg, sum_reg, file = "../../data/regression.RData")
