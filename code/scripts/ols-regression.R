dcbb90de3bf16b952b64da0cd0ab916b8739ad4a
PhilHoon Oh <vlfgns5@gmail.com>
2016-11-02 11:56:52 -0700

    editing ols-regression.R to use the results in Rmd files

--- code/scripts/ols-regression.R
+++ code/scripts/ols-regression.R
@@ -1,4 +1,3 @@
-load('../../data/new-train-test.RData')
 load('../../data/scale-train-test.RData')
 scaled_credit = read.csv("../../data/scaled-credit.csv")
 scaled_credit = scaled_credit[,-1]
@@ -20,11 +19,22 @@ sum_reg2 = summary(reg2)
 #using the result from reg2 only significant predictor variables from sum_reg2
 reg3 = lm(balance ~ income+limit+cards+student, data = scaled_credit_train)
 sum_reg3 = summary(reg3)
-plot(reg3)
 
 #since there is no significant difference between reg2 and reg3, going to use reg3 for our OLS model
+#calculate the test_mse
 ols.pred = predict(reg3, newdata = scaled_credit_test)
-mean((ols.pred - y.test)^2)
+test_mse_ols = mean((ols.pred - y.test)^2)
+
+#using the predictors in reg3, apply lm function to full data set
+reg4 = lm(balance ~ income+limit+cards+student, data = scaled_credit)
+sum_ols_full = summary(reg4)
+
+#calculate the full_data_mse
 ols.pred_full = predict(reg3, newdata = scaled_credit)
-mean((ols.pred_full - y.test_full)^2)
- 
+full_mse_ols = mean((ols.pred_full - y.test_full)^2)
+
+
+#saving the result of test_mse, official coefficients of full data, and full_data_mse
+save(test_mse_ols,sum_ols_full,full_mse_ols,file = "../../data/ols-regression.Rdata")
+
+
