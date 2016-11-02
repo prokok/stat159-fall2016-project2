15bad6a6b687078690bcc6bbe0449fb05c1252f2
PhilHoon Oh <vlfgns5@gmail.com>
2016-11-02 14:41:36 -0700

    renaming previous scripts becacuse of merging error

--- code/scripts/plsr-regression.R
+++ code/scripts/plsr-regression.R
@@ -1,5 +1,4 @@
 library(pls)
-load('../../data/new-train-test.RData')
 load('../../data/scale-train-test.RData')
 scaled_credit = read.csv("../../data/scaled-credit.csv")
 scaled_credit = scaled_credit[,-1]
@@ -7,21 +6,40 @@ scaled_credit = scaled_credit[,-1]
 y.test = scaled_credit_test$balance
 y.test_full = scaled_credit$balance
 
+#1 Run the corresponding fitting function on the train set using ten-fold cross-validation. 
 set.seed(0)
-pls.fit <- plsr(balance~., data = scaled_credit_train[,-1], validation='CV')
-summary(pls.fit)
-M <- which(pls.fit$validation$PRESS==min(pls.fit$validation$PRESS))
+pls.fit = plsr(balance~., data = scaled_credit_train, validation='CV')
 
+
+#2. Saving list of models(saved in Rdata file at the end)
+list_models_plsr = pls.fit
+
+
+#3. To select the best model:
+M = which(pls.fit$validation$PRESS==min(pls.fit$validation$PRESS))
+
+
+#4. Plot the cross-validation errors in terms of the tunning 
+##  parameter to visualize which parameter gives the "best" model:
+png(filename = "../../images/plsr-cross-validation-errors.png", width=800, height=600)
 validationplot(pls.fit, val.type="MSEP")
+dev.off()
 
-##code to subset pcr.fit$validation$PRESS to the 10th parameter
 
-#ncomp = m  (in this case m=10)
-pls.pred <- predict(pls.fit, scaled_credit_test, ncomp=M)
-#find the MSE
-mean((pls.pred - y.test)^2)
+#5. Once you identify the "best" model, use the test set to compute the test Mean Square Error(test MSE)
+pls.pred = predict(pls.fit, scaled_credit_test, ncomp=M)
+test_mse_plsr = mean((pls.pred - y.test)^2)
+
+#6. Last but not least, refit the model on the full data set using the parameter chosen by cross-validation.
+##  This fit will give you the "official" coefficient estimates.
+plsr_full = plsr(balance~., ncomp = M, data = scaled_credit)
+cof_plsr = plsr_full$coefficients[1:ncol(scaled_credit)-1,1,M]
 
 #full prediction
 pls.pred_full <- predict(pls.fit, scaled_credit, ncomp =M)
-mean((pls.pred_full - y.test_full)^2)
+full_mse_plsr = mean((pls.pred_full - y.test_full)^2)
 
+#saving the the best tunning parameter, list_models_plsr, test_mse_plsr, cof_plsr, full_mse_plsr
+par_plsr = M
+save(par_plsr,list_models_plsr, test_mse_plsr, cof_plsr, full_mse_plsr
+     , file = "../../data/plsr-regression.Rdata")
--- code/scripts/premodeling-data-processing.R
+++ code/scripts/premodeling-data-processing.R
@@ -32,7 +32,6 @@ new_credit =cbind(income, limit, rating, cards, age, education,
                   gender = new_gender[,1], student = new_student[,1]
                   , marriage=new_marriage[,1], asian = new_ethnicity[,1]
                   , caucasian = new_ethnicity[,2], balance = balance)
-write.csv(new_credit, file ="../../data/new-credit.csv")
 
 #Mean Centering and Standardizing 
 scaled_credit = scale(new_credit, center=TRUE, scale=TRUE)
